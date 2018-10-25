b67147783577496a25d27c4d799576015ed111da
reid-sutherland <reid.sutherland96@gmail.com>
2018-10-25 13:40:51 -0500

    Assignment2

--- Assignment2/main.c
+++ Assignment2/main.c
@@ -1,95 +1,27 @@
 #include "global.h"
 
-// Variables for main here
 
-enum SM1_States { SM1_init, SM1_coinIn, SM1_waitRise, SM1_waitChoice, SM1_choice, SM1_dispense } SM1_State;
+unsigned char WaitFlash_Clk;
+unsigned char GameOverFlash_Clk;
+unsigned char Debounce_Clk;
+unsigned char Game_Clk;
 
-TickFct_State_machine_1() {
-   switch(SM1_State) { // Transitions
-      case -1:
-         SM1_State = SM1_init;
-         break;
-         case SM1_init:
-         if (!A0) {
-            SM1_State = SM1_waitRise;
-         }
-         break;
-      case SM1_coinIn:
-         if (!A0) {
-            SM1_State = SM1_waitRise;
-         }
-         else if (coinCount == 4) {
-            SM1_State = SM1_waitChoice;
-         }
-         break;
-      case SM1_waitRise:
-         if (A0) {
-            SM1_State = SM1_coinIn;
-            coinCount++;
-         }
-         break;
-      case SM1_waitChoice:
-         if (A == 0x02 || A == 0x04 || A == 0x08 || A == 0x10 || A == 0x03 || A == 0x05 || A == 0x09 || A == 0x11) {
-            SM1_State = SM1_choice;
-         }
-         break;
-      case SM1_choice:
-         if (1) {
-            SM1_State = SM1_dispense;
-         }
-         break;
-      case SM1_dispense:
-         if (wait < 20) {
-            SM1_State = SM1_dispense;
-            wait++;
-         }
-         else if (wait == 20) {
-            SM1_State = SM1_init;
-            B = 0x00;
-         }
-         break;
-      default:
-         SM1_State = SM1_init;
-   } // Transitions
 
-   switch(SM1_State) { // State actions
-      case SM1_init:
-         coinCount = 0;
-         wait = 0;
-         break;
-      case SM1_coinIn:
-         break;
-      case SM1_waitRise:
-         break;
-      case SM1_waitChoice:
-         B0 = 1;
-         break;
-      case SM1_choice:
-         if (A1) {
-             B1 = 1;
-         }
-         else if (A2) {
-             B2 = 1;
-         }
-         else if (A3) {
-             B3 = 1;
-         }
-         else if (A4) {
-             B4 = 1;
-         }
-         break;
-      case SM1_dispense:
-         break;
-      default: // ADD default behaviour below
-      break;
-   } // State actions
-
+// Timer function
+void TimerISR() {
+   SM1_Clk = 1;
 }
 
 int main() {
 
-   const unsigned int periodState_machine_1 = 50;
-   TimerSet(periodState_machine_1);
+   const unsigned int WaitFlash_Period = 50;
+	 const unsigned int GameOverFlash_Period = 50;
+	 const unsigned int Debounce_Period = 20;
+	 const unsigned int Game_Period = 100;
+
+	 // Skip the math here, but should come out to a universal period of 10 ms
+	 const unsigned int Concurrent_Period = 10;
+   TimerSet(Concurrent_Period);
    TimerOn();
 
    SM1_State = -1; // Initial state
